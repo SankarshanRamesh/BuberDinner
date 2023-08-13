2bbabd7f593cd158cb6cc53f15eab065b7ac1c43
SankarshanRamesh <Sankarshanr@gmail.com>
2023-08-13 18:53:19 +0530

    added FluentValidation for RegisterCommand

---
+++ BuberDinner.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BuberDinner.Application.Authentication.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(obj => obj.FirstName).NotEmpty();
+        }
+    }
+}
--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using MediatR;
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -9,6 +14,9 @@ namespace BuberDinner.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            services.AddScoped<IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+                ValidationRegisterCommandBehavior>();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             return services;
         }
     }
